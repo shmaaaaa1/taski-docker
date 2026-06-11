003b0007b76d2eab69ce33d4d6106f69c182aade
shmaaaaa1 <kaverinandrej24@gmail.com>
2026-06-11 19:15:59 +0300

    Add Actions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,9 +62,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/data/db.sqlite3',
     }
 }
+
 
 
 # Password validation
@@ -104,6 +105,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
